fd88512d55d91c12ff428c145755642f15172dca
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2018-05-27 19:00:50 -0500

    apartment inlines in admin

--- condo/condapp/admin.py
+++ condo/condapp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(AptType)
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    extra = 1
+
+class FeeInline(admin.TabularInline):
+    model = Fee
+    extra = 1
+    
+class ApartmentAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline,
+               FeeInline, ]
+    list_filter = ('apt_type', )
+
+    list_display = ['number', 'apt_type' ]
+
+    
+admin.site.register(Apartment, ApartmentAdmin)
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(ProjectAttachment)
+admin.site.register(Quotation)
+admin.site.register(Volunteer)
+admin.site.register(Fee)
+admin.site.register(Payment)
--- condo/condapp/models.py
+++ condo/condapp/models.py
@@ -1,56 +1,63 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Appartment:
-    number = models.CharField(max_length=200)
+class AptType(models.Model):
+    name = models.CharField(max_length=20)
 
-class Profile:
+    def __str__(self):
+        return "tipo: " + self.name
+    
+class Apartment(models.Model):
+    number = models.CharField(max_length=200)
+    apt_type = models.ForeignKey(AptType, on_delete=models.CASCADE)
+    def __str__(self):
+        return "depto %s, (%s)" % (self.number, self.apt_type)
+
+class Profile(models.Model):
     phone = models.CharField(max_length=200)
-    appartment = models.OneToOneField(Appartment)
+    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
     owner = models.BooleanField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "%s - %s" % (self.user, self.apartment)
+
     
-class Project:
+class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    user = models.ForeignKey(User)        
-    budget = models.DecimalField(decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    budget = models.DecimalField(max_digits=19, decimal_places=2)
     creation_date = models.DateField(auto_now=True)
     due_date = models.DateField()
     delivered = models.BooleanField(default=False)
 
-class ProjectAttachment:
-    project = models.ForeignKey(Project)
+class ProjectAttachment(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     attachment = models.FileField()
     
-class Quotation:
+class Quotation(models.Model):
     provider = models.CharField(max_length=200)
-    user = models.ForeignKey(User)        
-    project = models.ForeignKey(Project)
-    price = models.DecimalField(decimal_places=2)
-    attachment = models.FileField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=19, decimal_places=2)
+    attachment = models.FileField(null=True, blank=True)
 
-class ProjectVote:
-    user = models.ForeignKey(User)    
-    project = models.ForeignKey(Project)
-    vote = models.BooleanField()
-
-class Volunteer:
-    user = models.ForeignKey(User)    
-    project = models.ForeignKey(Project)    
+class Volunteer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
 
-class Fee:
-    ammount = models.DecimalField(decimal_places=2)
-    appartment = models.ForeignKey(Appartment)
+class Fee(models.Model):
+    ammount = models.DecimalField(max_digits=19, decimal_places=2)
+    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
     due_date = models.DateField()
 
     def get_status(self):
         return "pendiente"
 
 
-class Payment:
-    fee = models.ForeignKey(Fee)    
+class Payment(models.Model):
+    fee = models.ForeignKey(Fee, on_delete=models.CASCADE)
     date = models.DateField(auto_now=True)
-    ammount = models.DecimalField(decimal_places=2)
+    ammount = models.DecimalField(max_digits=19, decimal_places=2)
 
