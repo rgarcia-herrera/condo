8c0a54e69a73a542cd380f73d4a7ba684b785b7f
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2018-05-27 19:34:31 -0500

    bare minimum admins for all tables

--- condo/condapp/admin.py
+++ condo/condapp/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(AptType)
-
 class ProfileInline(admin.TabularInline):
     model = Profile
     extra = 1
@@ -11,20 +9,56 @@ class ProfileInline(admin.TabularInline):
 class FeeInline(admin.TabularInline):
     model = Fee
     extra = 1
-    
+
 class ApartmentAdmin(admin.ModelAdmin):
     inlines = [ProfileInline,
                FeeInline, ]
+
     list_filter = ('apt_type', )
 
     list_display = ['number', 'apt_type' ]
 
-    
+
 admin.site.register(Apartment, ApartmentAdmin)
+
 admin.site.register(Profile)
-admin.site.register(Project)
-admin.site.register(ProjectAttachment)
-admin.site.register(Quotation)
-admin.site.register(Volunteer)
-admin.site.register(Fee)
-admin.site.register(Payment)
+
+
+class AttachmentInline(admin.TabularInline):
+    model = ProjectAttachment
+    extra = 1
+
+class QuotationInline(admin.TabularInline):
+    model = Quotation
+    extra = 1
+
+class VolunteerInline(admin.TabularInline):
+    model = Volunteer
+    extra = 1
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'budget', 'creation_date', 'due_date', 'delivered']
+
+    list_filter = ('due_date', 'delivered')
+    
+    inlines = [VolunteerInline,
+               QuotationInline,
+               AttachmentInline,]
+
+admin.site.register(Project, ProjectAdmin)
+
+class PaymentInline(admin.TabularInline):
+    model = Payment
+    extra = 1
+
+class FeeAdmin(admin.ModelAdmin):
+    inlines = [PaymentInline, ]
+
+    list_display = ['name', 'ammount', 'apartment', 'due_date', 'get_status']
+
+    list_filter = ('due_date', 'apartment')
+
+    
+admin.site.register(Fee, FeeAdmin)
+
+
--- condo/condapp/models.py
+++ condo/condapp/models.py
@@ -30,10 +30,16 @@ class Project(models.Model):
     creation_date = models.DateField(auto_now=True)
     due_date = models.DateField()
     delivered = models.BooleanField(default=False)
+    
+    def __str__(self):
+        delivered = "entregado" if self.delivered else ""
+        return "%s $%02f %s %s" % (self.name, self.budget, self.due_date, delivered)
 
+    
 class ProjectAttachment(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     attachment = models.FileField()
+    date = models.DateField(auto_now=True)    
     
 class Quotation(models.Model):
     provider = models.CharField(max_length=200)
@@ -41,6 +47,7 @@ class Quotation(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=19, decimal_places=2)
     attachment = models.FileField(null=True, blank=True)
+    date = models.DateField(auto_now=True)
 
 class Volunteer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -48,6 +55,7 @@ class Volunteer(models.Model):
 
 
 class Fee(models.Model):
+    name = models.CharField(max_length=200)    
     ammount = models.DecimalField(max_digits=19, decimal_places=2)
     apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
     due_date = models.DateField()
@@ -55,9 +63,14 @@ class Fee(models.Model):
     def get_status(self):
         return "pendiente"
 
+    def __str__(self):
+        return "%s %s %s %s" % (self.name, self.ammount, self.apartment, self.due_date)
 
 class Payment(models.Model):
     fee = models.ForeignKey(Fee, on_delete=models.CASCADE)
     date = models.DateField(auto_now=True)
     ammount = models.DecimalField(max_digits=19, decimal_places=2)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return "%s %s %s %s" % (self.date, self.fee, self.ammount, self.user)
