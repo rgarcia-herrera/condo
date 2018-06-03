1576a2082a2d96dfb86491825cf5c95ecd36893e
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2018-06-03 16:06:19 -0500

    mas avances a la traducción

--- condo/condapp/admin.py
+++ condo/condapp/admin.py
@@ -1,64 +1,71 @@
 from django.contrib import admin
+from .models import Profile, Fee, Unit, \
+    ProjectAttachment, Project, Quotation, Volunteer, Payment, \
+    UnitType
 
-from .models import *
 
 class ProfileInline(admin.TabularInline):
     model = Profile
     extra = 1
 
+
 class FeeInline(admin.TabularInline):
     model = Fee
     extra = 1
 
-class ApartmentAdmin(admin.ModelAdmin):
+
+class UnitAdmin(admin.ModelAdmin):
     inlines = [ProfileInline,
                FeeInline, ]
 
-    list_filter = ('apt_type', )
+    list_filter = ('unit_type', )
+    list_display = ['number', 'unit_type']
 
-    list_display = ['number', 'apt_type' ]
 
-
-admin.site.register(Apartment, ApartmentAdmin)
-
+admin.site.register(Unit, UnitAdmin)
 admin.site.register(Profile)
+admin.site.register(UnitType)
 
 
 class AttachmentInline(admin.TabularInline):
     model = ProjectAttachment
     extra = 1
 
+
 class QuotationInline(admin.TabularInline):
     model = Quotation
     extra = 1
+
 
 class VolunteerInline(admin.TabularInline):
     model = Volunteer
     extra = 1
 
+
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['name', 'user', 'budget', 'creation_date', 'due_date', 'delivered']
+    list_display = ['name', 'user', 'budget', 'creation_date',
+                    'due_date', 'delivered']
 
     list_filter = ('due_date', 'delivered')
-    
+
     inlines = [VolunteerInline,
                QuotationInline,
-               AttachmentInline,]
+               AttachmentInline, ]
+
 
 admin.site.register(Project, ProjectAdmin)
+
 
 class PaymentInline(admin.TabularInline):
     model = Payment
     extra = 1
 
+
 class FeeAdmin(admin.ModelAdmin):
     inlines = [PaymentInline, ]
 
-    list_display = ['name', 'ammount', 'apartment', 'due_date', 'get_status']
+    list_display = ['name', 'ammount', 'unit', 'due_date', 'get_status']
+    list_filter = ('due_date', 'unit')
 
-    list_filter = ('due_date', 'apartment')
 
-    
 admin.site.register(Fee, FeeAdmin)
-
-
--- condo/condapp/models.py
+++ condo/condapp/models.py
@@ -3,37 +3,37 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 
-class AptType(models.Model):
+class UnitType(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
-        return "tipo: " + self.name
+        return self.name
 
     class Meta:
-        verbose_name = _('Apartment type')
-        verbose_name_plural = _('Apartment types')
+        verbose_name = _('Unit type')
+        verbose_name_plural = _('Unit types')
 
 
-class Apartment(models.Model):
+class Unit(models.Model):
     number = models.CharField(_('number'),
                               max_length=200,
-                              help_text=_('Apartment number'))
-    apt_type = models.ForeignKey(AptType,
-                                 help_text=_('Type of apartment'),
-                                 on_delete=models.CASCADE)
+                              help_text=_('Room number'))
+    unit_type = models.ForeignKey(UnitType,
+                                  help_text=_('e.g. apartment, shop, etc.'),
+                                  on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _('Apartment')
         verbose_name_plural = _('Apartments')
 
     def __str__(self):
-        return _("apartment") + "%s, (%s)" % (self.number, self.apt_type)
+        return "%s %s" % (self.apt_type, self.number)
 
 
 class Profile(models.Model):
     phone = models.CharField(_('phone number'),
                              max_length=200)
-    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     owner = models.BooleanField(_('owner'))
     user = models.OneToOneField(User,
                                 verbose_name=_('user'),
@@ -41,6 +41,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return "%s - %s" % (self.user, self.apartment)
+
+    class Meta:
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profiles')
 
 
 class Project(models.Model):
@@ -64,11 +68,19 @@ class Project(models.Model):
         return "%s $%02f %s %s" % (self.name, self.budget,
                                    self.due_date, delivered)
 
+    class Meta:
+        verbose_name = _('Project')
+        verbose_name_plural = _('Projects')
+
 
 class ProjectAttachment(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     attachment = models.FileField()
     date = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('Attachment')
+        verbose_name_plural = _('Attachments')
 
 
 class Quotation(models.Model):
@@ -79,17 +91,29 @@ class Quotation(models.Model):
     attachment = models.FileField(null=True, blank=True)
     date = models.DateField(auto_now=True)
 
+    class Meta:
+        verbose_name = _('Quotation')
+        verbose_name_plural = _('Quotations')
+
 
 class Volunteer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = _('Voluntario')
+        verbose_name_plural = _('Voluntarios')
+
 
 class Fee(models.Model):
     name = models.CharField(max_length=200)
     ammount = models.DecimalField(max_digits=19, decimal_places=2)
-    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     due_date = models.DateField()
+
+    class Meta:
+        verbose_name = _('Fee')
+        verbose_name_plural = _('Fees')
 
     def get_status(self):
         return "pendiente"
@@ -107,3 +131,7 @@ class Payment(models.Model):
 
     def __str__(self):
         return "%s %s %s %s" % (self.date, self.fee, self.ammount, self.user)
+
+    class Meta:
+        verbose_name = _('Pago')
+        verbose_name_plural = _('Pagos')
